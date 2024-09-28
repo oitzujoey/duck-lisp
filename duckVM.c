e1c3581814fd18b4c40c6e47af0f5587844ea235
Joey Herguth <oitzujoey@gmail.com>
2024-09-28 11:33:02 -0700

    Change push-integer's negation to a sign extend.

--- duckVM.c
+++ duckVM.c
@@ -903,7 +903,7 @@ int duckVM_executeInstruction(duckVM_t *duckVM,
 		}
 
 		ptrdiff1 = (((dl_size_t) ptrdiff1 > size1)
-		            ? -((((dl_ptrdiff_t) size1)<<1)+0x2 - ptrdiff1)
+		            ? (dl_ptrdiff_t) (~size1 | (dl_size_t) ptrdiff1)
 		            : ptrdiff1);
 		object1 = duckVM_object_makeInteger(ptrdiff1);
 		e = stack_push(duckVM, &object1);
